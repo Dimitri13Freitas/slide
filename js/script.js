4410059da615abe17b03ef227d033e85c255c35c
Dimitri Freitas <freitasdimitri7@gmail.com>
2022-09-11 20:54:11 -0300

    segundo Slide em andamento

--- js/script.js
+++ js/script.js
@@ -1,50 +1,55 @@
-const img = document.querySelectorAll('[data-img="Primeiro"]');
-const btn = document.querySelectorAll('[data-btn="Primeiro"]');
+// Primeiro SLide
+function PrimeiroSlide() {
+  const img = document.querySelectorAll('[data-img="Primeiro"]');
+  const btn = document.querySelectorAll('[data-btn="Primeiro"]');
 
-img[0].classList.add('ativo');
-btn[0].classList.add('ativo');
+  img[0].classList.add('ativo');
+  btn[0].classList.add('ativo');
 
-let index = 0
+  let index = 0
 
-setInterval(() => {
-  img.forEach((e) => {
-    e.classList.remove('ativo')
-  });
-
-  img[index].classList.add('ativo')
-
-  btn.forEach((e) => {
-    e.classList.remove('ativo')
-  });
-  btn[index].classList.add('ativo')
-
-  index++
-  if (index >= img.length) {
-    index = 0
-  }
-}, 2000);
-
-
-btn.forEach((e, i) => {
-  e.addEventListener('click',() => {
-    btn.forEach((e) => {
-      e.classList.remove('ativo')
-    });
-
-    e.classList.add('ativo');
-
+  setInterval(() => {
     img.forEach((e) => {
       e.classList.remove('ativo')
     });
 
-    index = i
     img[index].classList.add('ativo')
+
+    btn.forEach((e) => {
+      e.classList.remove('ativo')
+    });
+    btn[index].classList.add('ativo')
+
+    index++
+    if (index >= img.length) {
+      index = 0
+    }
+  }, 3000);
+
+
+  btn.forEach((e, i) => {
+    e.addEventListener('click',() => {
+      btn.forEach((e) => {
+        e.classList.remove('ativo')
+      });
+
+      e.classList.add('ativo');
+
+      img.forEach((e) => {
+        e.classList.remove('ativo')
+      });
+
+      index = i;
+      img[index].classList.add('ativo');
+    });
   });
-});
-
-function teste() {
-  
 };
+PrimeiroSlide();
 
+// Segundo Slide
+
+const img = document.querySelectorAll('[data-img="Segundo"]');
+const btn = document.querySelectorAll('[data-btn="Segundo"]');
+
+console.log(img);
 console.log(btn);
-
